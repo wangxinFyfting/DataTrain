f049734366b517ff7f01bb26f801fcd1e49a3c75
wang xin <1275662089@qq.com>
2023-05-18 16:25:30 +0000

    Complete raw data processing in the form of RUL calculation

--- sales_data_processing.py
+++ sales_data_processing.py
@@ -1,23 +1,28 @@
 import pandas
-from datetime import datetime
+import datetime
 
 
 def sales_Data_RUL(data_path, warning_time):
+    # Obtain raw data and format the time
     data_form = pandas.read_csv(data_path)
     data_form["reasult"] = 0
     data_form["Time Point"] = pandas.to_datetime(data_form["Time Point"], format='%Y/%m/%d %H:%M')
+    
+    # Calculate how many pieces of data will be in the warning time
+    delta = data_form.loc[1, "Time Point"] - data_form.loc[0, "Time Point"]
+    delta_seconds = delta.total_seconds()
+    wtimes = warning_time / delta_seconds
+
+    # Collect all rows that exceed the Overall Index threshold 
+    # and reverse order them based on the index value
     failed_labe = data_form.loc[data_form["Overall Index (US)"] > 100]
     failed_labe = failed_labe.sort_values(by="Time Point", ascending=False)
-    print(failed_labe.head(5))
-    # sorted_df = data_form.sort_values(by="Time Point", ascending=False)
-    # sorted_df.tail(1)
-    # print(sorted_df.head(5))
-    # 将字符串转换为datetime对象
-    # date_str = '2022/1/22 0:26'
-    # date_obj = datetime.strptime(data_labe["Time Point"], '%Y/%m/%d %H:%M')
+    failed_num = failed_labe.shape[0]
+    # ~~~bug~~~ print(data_form.loc[failed_labe.index[1] -15 : failed_labe.index[1]])
 
-    # # 计算时间差
-    # diff = datetime.now() - date_obj
+    # Mark all data within the warning time
+    for i in range(0, failed_num):
+        data_form.loc[range(failed_labe.index[i] - wtimes, failed_labe.index[i] + 1), "reasult"] = 1
 
 if __name__ == '__main__':
-    sales_Data_RUL('Khery_366.csv', 10)
+    sales_Data_RUL('Khery_366.csv', 3)
