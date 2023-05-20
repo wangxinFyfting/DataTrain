bf338a7c18d43c7b0b5ceafb279df1f0223cc72f
wang xin <1275662089@qq.com>
2023-05-20 04:58:30 +0000

    Add the function of interval marking

--- sales_data_processing.py
+++ sales_data_processing.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy
 import datetime
 
 def slaes_wtimes(dForm, warnTime):
@@ -8,27 +9,46 @@ def slaes_wtimes(dForm, warnTime):
     wtimes = int(warnTime / delta_seconds)
     return wtimes
 
-def sales_Data_RUL(data_path, warning_time):
-    # Obtain raw data and format the time
-    data_form = pandas.read_csv(data_path)
-    data_form["reasult"] = 0
-    data_form["Time Point"] = pandas.to_datetime(data_form["Time Point"], format='%Y/%m/%d %H:%M')
+def label_data(data, pop, times):
 
-    wtimes = slaes_wtimes(data_form, warning_time)    
-
+    failed_form = pandas.DataFrame()
     # Collect all rows that exceed the Overall Index threshold 
     # and reverse order them based on the index value
-    failed_labe = data_form.loc[data_form["Overall Index (US)"] > 100]
-    failed_labe = failed_labe.sort_values(by="Time Point", ascending=False)
-    failed_num = failed_labe.shape[0]
-    # ~~~bug~~~ print(data_form.loc[failed_labe.index[1] -15 : failed_labe.index[1]])
+    for i in range(0, len(pop)):
+        if len(pop) == 1:
+            break
+        temp_labe = pandas.DataFrame()
+        temp_labe = data.loc[data["Overall Index (US)"] > pop[i] and data["Overall Index (US)"] < pop[i+1]].copy()
+        temp_labe.loc[:, "reasult"] = i + 1
+        failed_form = pandas.concat([failed_form, temp_labe], axis=0)
+        if i + 1 >= len(pop) - 1:
+            break
+
+    temp_labe = pandas.DataFrame()
+    temp_labe = data.loc[data["Overall Index (US)"] > pop[-1]].copy()
+    temp_labe.loc[:, "reasult"] = len(pop)
+    failed_form = pandas.concat([failed_form, temp_labe], axis=0)
+
+    failed_form = failed_form.sort_values(by="Time Point", ascending=False)
+    failed_num = failed_form.shape[0]
+    # ~~~bug~~~ print(data.loc[failed_labe.index[1] -15 : failed_labe.index[1]])
 
     # Mark all data within the warning time
+    data["reasult"] = 0
     for i in range(0, failed_num):
-        data_form.loc[range(failed_labe.index[i] - wtimes, failed_labe.index[i] + 1), "reasult"] = 1
-    # data_form.to_csv('newRULData.csv')
+        index = failed_form.index[i]
+        data.loc[range(index - times, index + 1), "reasult"] = failed_form.loc[index, "reasult"]
 
-def sales_data_timeShift(data_path, warning_time):
+def sales_Data_RUL(data_path, warning_time, pop):
+    # Obtain raw data and format the time
+    data_form = pandas.read_csv(data_path)
+    data_form["Time Point"] = pandas.to_datetime(data_form["Time Point"], format='%Y/%m/%d %H:%M')
+    wtimes = slaes_wtimes(data_form, warning_time)    
+    label_data(data_form, pop, wtimes)
+
+    data_form.to_csv('newRULData.csv')
+
+def sales_data_timeShift(data_path, warning_time, pop):
     # Obtain raw data and format the time
     data_form = pandas.read_csv(data_path)
     data_form["Time Point"] = pandas.to_datetime(data_form["Time Point"], format='%Y/%m/%d %H:%M')
@@ -45,8 +65,10 @@ def sales_data_timeShift(data_path, warning_time):
 
     data_form = pandas.concat([data_form, y], axis=1)
     data_form = data_form.reset_index(drop=True)
-    # data_form.to_csv('newTimeShift.csv')
+
+    label_data(data_form, pop, wtimes)
+    data_form.to_csv('newTimeShift.csv')
 
 if __name__ == '__main__':
-    sales_Data_RUL('Khery_366.csv', 600)
-    sales_data_timeShift('Khery_366.csv', 600)
+    sales_Data_RUL('Khery_366.csv', 600, numpy.array([100]))
+    sales_data_timeShift('Khery_366.csv', 600, numpy.array([100]))
