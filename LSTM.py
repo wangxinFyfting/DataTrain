ec5b01a6e7fcf3240d703492a6d915ef8abd1f89
wang xin <1275662089@qq.com>
2023-05-30 04:22:54 +0000

    tarin LSTM results

--- LSTM.py
+++ LSTM.py
@@ -6,12 +6,15 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import load_model
 from sklearn.model_selection import train_test_split
 import numpy
+import pandas
+import matplotlib.pyplot as plt
 def Stock_Price_LSTM_Data_Precesing(df, mem_his_days, pre_days):
     df.dropna(inplace=True)
     df.sort_index(inplace=True)
-    df['label'] = df['Close'].shift(-pre_days)
+    # df['Overall Index (US)'] = df['Overall Index (US)'].shift(-pre_days)
     scaler = StandardScaler()
     scal_X = scaler.fit_transform(df.iloc[:,:-1])
     deq = deque(maxlen=mem_his_days)
@@ -23,39 +26,44 @@ def Stock_Price_LSTM_Data_Precesing(df, mem_his_days, pre_days):
     X_lately = X[-pre_days:]
     X = X[:-pre_days]
 
-    y = df['label'].values[mem_his_days-1:-pre_days]
+    y = df['Overall Index (US)'].values[mem_his_days-1:-pre_days]
     
     X = numpy.array(X)
     y = numpy.array(y)
 
     return X, y, X_lately
 
-# X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
-# model = Sequential()
-
-# model.add(LSTM(10, input_shape=x.shape[1:], activation='relu', return_sequences=True))
-# model.add(Dropout(0.1))
-# model.add(LSTM(10, activation='relu', return_sequences=True))
-# model.add(Dropout(0.1))
-# model.add(LSTM(10, activation='relu'))
-# model.add(Dropout(0.1))
-# model.add(Dense(10, activation='relu'))
-# model.add(Dropout(0.1))
-# model.compile(optimizer='adam', loss='mse', metrics=['mape'])
-# model.fit(X_train, y_train, batch_size=32, epochs=50, validation_data=(X_test, y_test))
-# y_pred = model.predict(X_test)
-
-
 if __name__ == '__main__':
 
-    start = datetime.datetime(2000, 1, 1)
-    end = datetime.datetime(2021, 9, 1)
-    df = web.DataReader('GOOGL', 'stooq', start, end)
+    # df = pandas.read_csv('./data_csv/RUL/RUL_label_100_warningtime600.csv')
+    # df = df.loc[0:3300, :]
+    # df.drop(["Time Point"], axis = 1, inplace=True)
+    # df.drop(["Primary Pollutant"], axis = 1, inplace=True)
+    # df.drop(["label"], axis = 1, inplace=True)
+    # # print(df.head(10))
+
+    # X, y, X_lately = Stock_Price_LSTM_Data_Precesing(df, 5, 10)
+    # best_model = load_model("./models/32.62_05_mem_5_lstm_1_dense_1_unit_32")
+    # pre = best_model.predict(X)
+    # df.reset_index(drop=True)
+    # plt.plot(df.index[-len(y):], y, color='red', label='trueData')
+    # plt.plot(df.index[-len(y):], pre, color='green', label='predictData')
+    # plt.savefig('lstm.png', bbox_inches='tight')    
+
+
+    df = pandas.read_csv('./data_csv/RUL/RUL_label_100_warningtime600.csv')
+    df = df.loc[0:3300, :]
+    df.drop(["Time Point"], axis = 1, inplace=True)
+    df.drop(["Primary Pollutant"], axis = 1, inplace=True)
+    df.drop(["label"], axis = 1, inplace=True)
+    df_train = df.loc[0:3000,:]
+    df_test = df.loc[3000:,:]
+
     pre_days =10
-    mem_days = [5, 10, 15]
-    lstm_layers = [1, 2, 3]
-    dense_layers = [1, 2, 3]
-    uints = [8, 16, 32]
+    mem_days = [5]
+    lstm_layers = [1]
+    dense_layers = [1]
+    uints = [32]
     for the_mem_days in mem_days:
         for the_lstm_layers in lstm_layers:
             for the_dense_layers in dense_layers:
@@ -63,15 +71,15 @@ if __name__ == '__main__':
                     filepath = './models/{val_loss:.2f}_{epoch:02d}_' + f'mem_{the_mem_days}_lstm_{the_lstm_layers}_dense_{the_dense_layers}_unit_{the_uints}'
                     checkpoint = ModelCheckpoint(
                         filepath = filepath,
-                        save_weights_only = True,
+                        save_weights_only = False,
                         monitor = "val_mape",
                         mode = "min",
                         save_best_only = True)
                     
-                    X, y, X_lately = Stock_Price_LSTM_Data_Precesing(df, the_mem_days, pre_days)
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.1)
+                    X_train, y_train, _ = Stock_Price_LSTM_Data_Precesing(df_train, the_mem_days, pre_days)
+                    X_test, y_test, _ = Stock_Price_LSTM_Data_Precesing(df_test, the_mem_days, pre_days)
                     model = Sequential()
-                    model.add(LSTM(the_uints, input_shape=X.shape[1:], activation='relu', return_sequences=True))
+                    model.add(LSTM(the_uints, input_shape=X_train.shape[1:], activation='relu', return_sequences=True))
                     model.add(Dropout(0.1))
 
                     for i in range(the_lstm_layers):
@@ -88,4 +96,9 @@ if __name__ == '__main__':
                     model.add(Dense(1))
                     model.compile(optimizer='adam', loss='mse', metrics=['mape'])
                     model.fit(X_train, y_train, batch_size=32, epochs=50, validation_data=(X_test, y_test), callbacks=[checkpoint])
-                    # y_pred = model.predict(X_test)
+
+                    y_pred = model.predict(X_test)
+                    df_test.reset_index(drop=True)
+                    plt.plot(df.index[-len(y_test):], y_test, color='red', label='trueData')
+                    plt.plot(df.index[-len(y_test):], y_pred, color='green', label='predictData')
+                    plt.savefig('lstm.png', bbox_inches='tight')
